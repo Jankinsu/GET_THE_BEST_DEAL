e3b8982e47227dfbd0ffeaaf48f816ee1b955475
Jankinsu <51951944+Jankinsu@users.noreply.github.com>
2021-11-12 10:07:02 +0800

    Update ddxpsum.js
    Revise patameter

--- ddxpsum.js
+++ ddxpsum.js
@@ -53,8 +53,8 @@ let ddxpsumhd = $.getdata('ddxpsumhd')
 
                 $.index = i + 1;
                 console.log(`\n\n开始【叮咚鱼塘整合${$.index}】`)
-				setp();
-				await rcqd(ddxpsumurl)
+				setp(ddxpsumurl);
+				await rcqd()
 				// 随机休息1-3秒
 				await $.wait(1000*(1+ Math.round(2*Math.random())))
 				await lxqd()
